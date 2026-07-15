2bbffb18ed66bccbba89cfc5b95649cc9f3e05be
Sheyda Rahmanpour <sheydarmp@gmail.com>
2026-07-15 14:44:00 +0330

    feat: implement player data model

---
+++ src/player.rs
@@ -0,0 +1,25 @@
+use crate::color::ColorRGB;
+
+pub struct Player {
+    pub name: String,
+    pub score: u32,
+    pub guess: ColorRGB,
+}
+impl Player {
+    pub fn new(name: &str) -> Self {
+        Self {
+            name: name.to_string(),
+            score: 0,
+            guess: ColorRGB::new(128, 128, 128),
+        }
+    }
+    pub fn set_guess(&mut self, color: ColorRGB) {
+    self.guess = color;
+}
+pub fn add_score(&mut self, points: u32) {
+    self.score += points;
+}
+pub fn reset_guess(&mut self) {
+    self.guess = ColorRGB::new(128, 128, 128);
+}
+}
