ec87389df17e9b24b53ce07f3666d6eb4a589543
Sheyda Rahmanpour <sheydarmp@gmail.com>
2026-07-19 11:38:01 +0330

    feat: add multiplayer network protocol packets

--- multiplayer/protocol.rs
+++ multiplayer/protocol.rs
@@ -1,3 +1,35 @@
-pub enum Protocol {
+use serde::{Deserialize, Serialize};
 
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub enum Packet {
+    Join {
+        name: String,
+    },
+
+    Ready,
+
+    Guess {
+        r: u8,
+        y: u8,
+        b: u8,
+    },
+
+    Hint,
+
+    Leave,
+
+    StartGame,
+
+    TargetColor {
+        r: u8,
+        y: u8,
+        b: u8,
+    },
+
+    Winner {
+        name: String,
+        similarity: f32,
+    },
+
+    TimeUp,
 }
